766cae539edcc3fe980e7137feeb107f0f0885ec
KeepFocusL <1281438594@qq.com>
2024-08-05 16:22:02 +0800

    在 pom.xml 下载 Jsoup 依赖

---
+++ src/main/java/com/day240825/JsoupTest.java
@@ -0,0 +1,14 @@
+package com.day240825;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class JsoupTest {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.baidu.com/").get();
+        //获取 baidu 官网首页的 html 源代码
+        System.out.println(doc.html());
+    }
+}
