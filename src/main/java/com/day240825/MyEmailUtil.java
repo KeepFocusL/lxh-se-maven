b12c96e87a602a50708104ab690fd3dc3e781532
KeepFocusL <1281438594@qq.com>
2024-08-05 16:48:20 +0800

    验证邮箱发送功能

---
+++ src/main/java/com/day240825/MyEmailUtil.java
@@ -0,0 +1,51 @@
+package com.day240825;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+
+import java.util.Properties;
+
+public class MyEmailUtil {
+    // 1. state (filed、variable)
+    private static final String FROM = "1281438594@qq.com"; // 邮件发送者的身份
+    private static final String SECRET_TOKEN = "ypjwdejyqxrpieab";//邮箱的密钥
+
+    // 2. behavior (method、function)
+    public static void send(String email, String content) throws Exception {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.qq.com");
+        prop.put("mail.smtp.port", "587");
+
+        // https://wx.mail.qq.com/list/readtemplate?name=app_intro.html#/agreement/authorizationCode
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(FROM, SECRET_TOKEN);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        // who you are
+        message.setFrom(new InternetAddress(FROM));
+        // send to ...
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+
+        message.setSubject("来自 Java 的提醒");
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(content, "text/html; charset=utf-8");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+
+        System.out.println("邮件发送成功");
+    }
+
+    // 3. constructor
+}
---
+++ src/main/java/com/day240825/MyEmailUtilTest.java
@@ -0,0 +1,7 @@
+package com.day240825;
+
+public class MyEmailUtilTest {
+    public static void main(String[] args) throws Exception {
+        MyEmailUtil.send("1281438594@qq.com", "这是一段测试消息");//收件人的邮箱
+    }
+}
