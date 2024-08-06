13dad4bcfa8ab66487af4f918bcafb5a9dedf5dd
KeepFocusL <1281438594@qq.com>
2024-08-06 13:49:19 +0800

    Update Student

--- src/main/java/day240806/Student.java
+++ src/main/java/day240806/Student.java
@@ -1,35 +1,12 @@
 package day240806;
 
+
 public class Student {
     //state
     String name;
     int age;
 
     //constructor
-    public Student() {
-    }
-
-    public Student(String newName, int age) {
-        this.name = newName;
-        this.age = age;
-
-    }
 
     //behavior
-
-    public String getNewName() {
-        return name;
-    }
-
-    public void setNewName(String newName) {
-        this.name = newName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
