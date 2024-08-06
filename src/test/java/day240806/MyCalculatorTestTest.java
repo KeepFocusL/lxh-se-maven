a5d43b822abef47eb08bd0cb00e9c41efe0233f7
KeepFocusL <1281438594@qq.com>
2024-08-06 16:58:45 +0800

    JUnit5 - @Disabled

--- src/test/java/day240806/MyCalculatorTestTest.java
+++ src/test/java/day240806/MyCalculatorTestTest.java
@@ -1,9 +1,11 @@
 package day240806;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 class MyCalculatorTestTest {
     @Test
     void add() {
