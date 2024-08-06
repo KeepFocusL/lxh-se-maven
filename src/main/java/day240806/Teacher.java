ee2be56aa8f345f45a01284f876f32ffe718636a
KeepFocusL <1281438594@qq.com>
2024-08-06 15:36:05 +0800

    由 lombok 引出 record 关键字...
    写代码 体会官方出的record新特性(灵感来自lombok)

---
+++ src/main/java/day240806/Teacher.java
@@ -0,0 +1,4 @@
+package day240806;
+
+public record Teacher(String name,int age) {
+}
---
+++ src/main/java/day240806/TeacherTest.java
@@ -0,0 +1,9 @@
+package day240806;
+
+public class TeacherTest {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher("老王",40);
+        System.out.println(teacher.name());
+        System.out.println(teacher.age());
+    }
+}
