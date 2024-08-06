5d5c771785e460a6e4751328e57f1a267004dfee
KeepFocusL <1281438594@qq.com>
2024-08-06 13:52:14 +0800

    Student + lombok 在编译的过程中自动生成 constructor 和 behavior...
    从 .java 到 .class 的途中生成

--- src/main/java/day240806/Student.java
+++ src/main/java/day240806/Student.java
@@ -1,6 +1,14 @@
 package day240806;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     //state
     String name;
