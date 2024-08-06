73c6418f3224cd36956cd5c30e657b4af6448540
KeepFocusL <1281438594@qq.com>
2024-08-06 16:49:24 +0800

    增加MyCalculatorTest

---
+++ src/main/java/day240806/MyCalculatorTest.java
@@ -0,0 +1,8 @@
+package day240806;
+
+public class MyCalculatorTest {
+    public static void main(String[] args) {
+        int sum = MyCalculator.add(2, 3);
+        System.out.println(sum == 5);
+    }
+}
